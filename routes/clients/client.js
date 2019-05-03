e721d926ed8e3c3c14400d132a86fb79a2e2e69f
AntoninVenuti <antonin.vnt@hotmail.com>
2019-05-03 15:00:38 +0200

    test validation champs

--- routes/clients/client.js
+++ routes/clients/client.js
@@ -1,7 +1,9 @@
 let express = require('express')
+let app = express();
 let router = express.Router()
 let clients = require('./controllers/clientController')
 let validation = require('./controllers/clientValidator')
+let { check } = require('express-validator/check')
 
 // Routes vers le CRUD clients
 
@@ -15,7 +17,17 @@ router.delete('/:id', clients.deleteClient);
 
 //Routes vers clientValidator
 
-router.post('/test', validation.validation);
+router.post('/test', validation.create);
+
+/*router.post('/test', [
+    check('lastName').isLength({ min: 3 }),
+    check('firstName').isLength({ min: 5}),
+    check('mail').isEmail()
+  ], (req, res) => {
+    const lastName  = req.body.lastName
+    const firstName = req.body.firstName
+    const mail = req.body.mail
+  })*/
 
 
 
--- routes/clients/controllers/clientValidator.js
+++ routes/clients/controllers/clientValidator.js
@@ -1,19 +1,76 @@
 let express = require('express')
-let { check, validationResult } = require('express-validator/check')
+let { check } = require('express-validator/check')
+let waterfall = require('async-waterfall');
+let regexMail = /^[a-zA-Z0-9._-]+@[a-z0-9._-]{2,}\.[a-z]{2,4}$/
 
-exports.validation = function (req,res,next) {
+/*exports.validation = function (req,res,next) {
     
-    if (check(req.body.lastName).isLength({min: 5})){
-    /*if(req.body.lastName === undefined || req.body.lastName === ""){
+    if(req.body.lastName === undefined || req.body.lastName === ""){
         res.send("Veuillez renseigner votre nom de famille")        
     } else { 
     res.send('Votre nom de famille est renseigné')
     console.log(req.body.lastName)
-    next();*/
+    next();
     res.send('plus long que deux')
     } else {
         res.send('trop court (moins que deux)')
     }
+    }
+}*/
 
+exports.create = function (req, res, next) {
+    waterfall([
+        truc,
+        
+        function testlastName (callback) {
+            if(req.body.lastName === undefined || req.body.lastName === ""){
+                console.log("pas de lastName")
+                callback('erreur')        
+            } else { 
+            console.log(req.body.lastName)
+            callback(null)
+            }
+        },
+        function testfirstName (callback) {
+            if(req.body.firstName === undefined || req.body.firstName === ""){
+                console.log("pas de firstName")
+                callback('erreur')     
+            } else { 
+            console.log(req.body.firstName)
+            callback(null)
+            }
+        },
+        function testMail(callback) {
+            if(req.body.mail === undefined || req.body.mail === ""){
+                console.log("pas de mail")
+                callback('erreur')   
+            } else { 
+            console.log(req.body.mail)
+            callback(null)
+            }
+        },
+        function testRegexMail(callback) {
+            if(!regexMail.test(req.body.mail)){
+            //if(check(req.body.mail).isEmail()){
+                console.log("adresse mail non valide")
+                callback('erreur')   
+            } else { 
+                console.log(req.body.mail)
+                callback(null)
+            }
+        },
+        
+    ], function(error) {
+        if (error) {
+            res.status(400).send(error);
+        }else {
+            console.log('ICI');
+            res.status(200).send("tout est correct");
+        }
+    })
+}
+
+function truc(callback) {
+    callback(null);
 }
 
